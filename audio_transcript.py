9aec42e81cbb09924823a750e0274bb58295e025
Prince Yadav <prince.yadav@tothenew.com>
2023-08-28 14:01:14 +0530

    aaled print statement

--- audio_transcript.py
+++ audio_transcript.py
@@ -28,7 +28,9 @@ def upload_file_to_s3(file_path, bucket_name, object_name=None):
     s3_client = session.client('s3')   
         
     try:
+        print(f"Uploading : {object_name}\n")
         s3_client.upload_file(file_path, bucket_name, object_name)
+        print(f"Finished Uploading : {object_name}\n")
         return True
     except Exception as e:
         print(f"Error uploading file: {e}")
